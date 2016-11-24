691c7cb61d171c7cd73ab7713746d358bfcf4b97
Roman Dittert <romd86@gmail.com>
2016-11-24 22:37:57 +0100

    Update components

--- src/app/mmr-brackets.ts
+++ src/app/mmr-brackets.ts
@@ -1,5 +1,5 @@
 export const MMR_BRACKETS = [
-    {threshold: 1149, name: 'amateur',      label: 'Amateur'},
+    {threshold: 1099, name: 'amateur',      label: 'Amateur'},
     {threshold: 1299, name: 'semi-pro',     label: 'Semi-Pro'},
     {threshold: 1499, name: 'professional', label: 'Professional'},
     {threshold: 1699, name: 'world-class',  label: 'World Class'},
